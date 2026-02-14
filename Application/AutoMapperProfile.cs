3c0aa1b6bcaa52689907786f6b690686d749ef1f
Omar Emad Abusaif <oabuseif125@gmail.com>
2026-02-14 04:59:07 +0200

    Make configrations that common between all branches - Introduced new entity classes: `ApiLog`, `FeatureFlag`, `SubscriptionPlan`, and `Tenant`, inheriting from `BaseEntity`. - Added migration files to create corresponding database tables for the new entities. - Enhanced `ApplicationDbContext` with DbSet properties for new entities. - Added Fluent API configuration classes to define database schema and relationships. - Introduced `GlobalApiResponse` and `ExceptionMiddleware` for improved error handling and API response formatting.

---
+++ Application/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿//using Application.DTOS;
+
+//using AutoMapper;
+
+//using Domain.Entites;
+
+//namespace Application;
+
+//public class AutoMapperProfile : Profile
+//{
+//    public AutoMapperProfile()
+//    {
+//        CreateMap<DataBatch, UploadStatusDto>();
+//        CreateMap<FinancialRecord, FinancialRecordDto>().ReverseMap();
+
+//    }
+//}
+
---
+++ Infra/Extentions/DependenciesCollector.cs
@@ -0,0 +1,35 @@
+﻿using Application;
+
+using FluentValidation;
+
+using Infrastructure.Persistence;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+namespace Infrastructure.Extentions;
+public static class DependenciesCollector
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        var assembly = typeof(IApplicationHandlerMarker).Assembly;
+        var DatabaseConfig = Environment.GetEnvironmentVariable("SaasDatabaseConfig");
+        services.AddDbContext<ApplicationDbContext>
+         (options =>
+         {
+             options.UseNpgsql((DatabaseConfig));
+         });
+        //services.AddScoped<IAI_AnalyticsServices, AI_AnalyticsServices>();
+        //services.AddHttpClient<IAI_InsightService, Ai_InsightService>();
+        //services.AddScoped<IUnitofWork, UnitofWork>();
+        //services.AddScoped<IData_InegstionService, Data_InegstionService>();
+        #region Mediator
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+        services.AddValidatorsFromAssembly(assembly);
+        #endregion
+        return services;
+    }
+}
